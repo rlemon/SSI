e3446c26252ac5abb8508a90e9f72071baeb5112
rlemon <rob.lemon@gmail.com>
2011-12-21 09:34:41 -0500

    pulled ui css and images from CDN for development

--- views/header.php
+++ views/header.php
@@ -3,7 +3,7 @@
 <head>
 	<title>SSI</title>
 
-	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/base/jquery-ui.css" />
+	<link rel="stylesheet" href="<?php echo URL; ?>public/css/jquery-ui.css" />
 	<link rel="stylesheet" href="<?php echo URL; ?>public/css/default.css" />
 </head>
 <body>
