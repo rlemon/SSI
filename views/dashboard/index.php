84cd3d4a43ada68c1635dd7539d47a282340cb14
rlemon <rob.lemon@gmail.com>
2012-01-04 15:13:33 -0500

    building dashboard options

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -3,19 +3,61 @@
 	<div class="ui-padded-bottom ui-heading">
 		<?php echo Session::get('username'); ?>'s Dashboard
 	</div>
-	<div class="ui-padded-bottom">
-	<span class="label small">Theme Selection:</span>
-	<select class="ui-state-default" name="theme">
-		<?php
-			for($i = 0, $l = count($this->themes); $i < $l; $i++) {
-				$selected = '';
-				if( Session::get('ui_theme') == $i ) {
-					$selected .= 'selected="selected" ';
-				}
-				echo '<option ' . $selected . 'value="' . $i . '">' . $this->themes[$i] . '</option>';
-			}
-		?>
-	</select>
+	<div class="ui-widget ui-padded-bottom">
+		<div class="ui-widget-header ui-padded-all">Personal Options</div>
+		<div class="ui-widget-content ui-padded-all">
+			<div class="ui-padded-bottom">
+				<table>
+					<tr>
+						<td>
+							<span class="small">Theme Selection:</span>
+						</td>
+						<td>
+							<select class="ui-state-default" name="theme">
+								<?php
+									for($i = 0, $l = count($this->themes); $i < $l; $i++) {
+										$selected = '';
+										if( Session::get('ui_theme') == $i ) {
+											$selected .= 'selected="selected" ';
+										}
+										echo '<option ' . $selected . 'value="' . $i . '">' . $this->themes[$i] . '</option>';
+									}
+								?>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2">
+							<span class="ui-padded-bottom">Change Password</span>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span class="small">Old Password:</span>
+						</td>
+						<td>
+							<input type="password" name="old_password" />
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span class="small">New Password:</span>
+						</td>
+						<td>
+							<input type="password" name="new_password" />
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span class="small">Confirm New Password:</span>
+						</td>
+						<td>
+							<input type="password" name="confirm_password" />
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
 	</div>
 	<input type="submit" class="ui-btn" value="Save" />
 </div>
